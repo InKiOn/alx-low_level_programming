ea7022ee20a892b1b6213ed5fed166063675642e
InKiOn <122981923+InKiOn@users.noreply.github.com>
2023-02-16 12:09:43 +0000

    Unix write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,15 +3,15 @@
  *
  * Description: Prints without printf and puts
  *
- * Return: Always 0 (success)
+ * Return: Always 1 (error)
  */
 
-#include <stdio.h>
+#include <unistd.h>
 
 int main(void)
 {
-	char mess = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *mess = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(mess, 1, strlen(mess), stderr);
-	return (0);
+	write(2, message, 59);
+	return (1);
 }
