bc5faf37a0981d1d76340a920875be370b8b4054
InKiOn <122981923+InKiOn@users.noreply.github.com>
2023-02-16 12:13:15 +0000

    Unix write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,8 @@
 
 int main(void)
 {
-	char *message= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *mess = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, message, 59);
+	write(2, mess, 59);
 	return (1);
 }
