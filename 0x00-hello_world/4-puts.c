87dfd0530fff7fca8350741a7a579e0f48d99c5d
InKiOn <122981923+InKiOn@users.noreply.github.com>
2023-02-16 10:09:14 +0000

    Betty documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
+/**
+ * main - entry point
+ *
+ * Description: prints line using puts
+ *
+ * Return: 0 on success
+ */
+
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
