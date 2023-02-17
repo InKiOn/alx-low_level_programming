729bbd321fd120f1e0d12101b2520d00e9508eb4
InKiOn <122981923+InKiOn@users.noreply.github.com>
2023-02-17 14:17:06 +0000

    Code cleanup

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,9 +15,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 99; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = i + j; j < 99; j++)
+		for (j = i + j; j < 100; j++)
 		{
 			if (j != i && i < j)
 			{
@@ -34,9 +34,10 @@ int main(void)
 				putchar(' ');
 			}
 		}
-	}	
+	}
 
 	putchar('\n');
 
 	return (0);
+	O
 }
