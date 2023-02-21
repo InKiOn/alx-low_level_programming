77cdaae9b53f49907628864e33fa5da4392eab32
InKiOn <122981923+InKiOn@users.noreply.github.com>
2023-02-21 14:59:39 +0000

    OUTPUT case fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,22 +13,29 @@ void print_times_table(int n)
 	if (n <= 15 && n >= 0)
 	{
 		int r, c, multiple;
-		
+
 		for (r = 0; r <= n; r++)
 		{
 			_putchar('0');
+
 			for (c = 1; c <= n; c++)
 			{
 				multiple = r * c;
 
-				_putchar(',');
-				_putchar(' ');
-
+				if (multiple <= 99)
+				{
+					_putchar(' ');
+				}
 				if (multiple <= 9)
 				{
 					_putchar(' ');
 				}
-				else
+				if (multiple >= 100)
+				{
+					_putchar((multiple / 100) + '0');
+					_putchar((multiple / 10) % 10 + '0');
+				}
+				else if (multiple <= 99 && multiple >= 10)
 				{
 					_putchar((multiple / 10) + '0');
 				}
