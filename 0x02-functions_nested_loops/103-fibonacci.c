193ccf8a0c3d3494ed27e39ec9192df89c2c8a16
InKiOn <122981923+InKiOn@users.noreply.github.com>
2023-02-21 15:32:44 +0000

    Fibonacci loops

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: Prints sum of even Fibonacci numbers
+ * 
+ * Return: 0.
+ */
+
+int main(void)
+{
+	float total;
+	unsigned long a = 0, b = 1, sum;
+	
+	while (1)
+	{
+		sum = a + b;
+
+		if (sum > 4000000)
+		{
+			break;
+		}
+
+		if ((sum % 2) == 0)
+		{
+			total += sum;
+		}
+
+		a = b;
+		b = sum;
+	}
+	printf("%.0f\n", total);
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Prints the first 98 Fibonacci numbers, starting with 1 and 2
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int count;
+	unsigned long a = 0, b = 1, sum;
+	unsigned long a_p1, a_p2, b_p1, b_p2;
+	unsigned long p1, p2;
+
+	for (count = 0; count < 92; count++)
+	{
+		sum = a + b;
+		printf("%lu, ", sum);
+
+		a = b;
+		b = sum;
+	}
+
+	a_p1 = a / 10000000000;
+	b_p1 = b / 10000000000;
+	a_p2 = a % 10000000000;
+	b_p2 = b % 10000000000;
+
+	for (count = 93; count < 99; count++)
+	{
+		p1 = a_p1 + a_p1;
+		p2 = a_p2 + b_p2;
+		if (a_p2 + b_p > 9999999999)
+		{
+			p1 += 1;
+			p2 %= 10000000000;
+		}
+
+		printf("%lu%lu", p1, p2);
+		if (count != 98)
+			printf(", ");
+
+		a_p1 = b_p1;
+		a_p2 = b_p2;
+		b_p1 = p1;
+		b_p2 = p2;
+	}
+	printf("\n");
+	return (0);
+}
