aedb6c4d342ee059c474804fd8b7bcfbefba9d02
InKiOn <122981923+InKiOn@users.noreply.github.com>
2023-02-21 17:10:04 +0000

    More fibonaccis

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,42 +12,15 @@ int main(void)
 {
 	int count;
 	unsigned long a = 0, b = 1, sum;
-	unsigned long a_p1, a_p2, b_p1, b_p2;
-	unsigned long p1, p2;
 
-	for (count = 0; count < 92; count++)
+	for (count = 1; count < 98; count++)
 	{
 		sum = a + b;
-		printf("%lu, ", sum);
 
 		a = b;
 		b = sum;
 	}
 
-	a_p1 = a / 10000000000;
-	b_p1 = b / 10000000000;
-	a_p2 = a % 10000000000;
-	b_p2 = b % 10000000000;
-
-	for (count = 93; count < 99; count++)
-	{
-		p1 = a_p1 + a_p1;
-		p2 = a_p2 + b_p2;
-		if (a_p2 + b_p2 > 9999999999)
-		{
-			p1 += 1;
-			p2 %= 10000000000;
-		}
-
-		printf("%lu%lu", p1, p2);
-		if (count != 98)
-			printf(", ");
-
-		a_p1 = b_p1;
-		a_p2 = b_p2;
-		b_p1 = p1;
-		b_p2 = p2;
-	}
 	printf("\n");
 	return (0);
 }
