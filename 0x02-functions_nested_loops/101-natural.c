f4af00434d58393bcbbd103202cec4cd5f591289
InKiOn <122981923+InKiOn@users.noreply.github.com>
2023-02-21 16:50:42 +0000

    Betty checeker error fix

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,23 +2,22 @@
 
 /**
  * main - start function
- * 
- * Description:  Lists all natural numbers below 1024 (excluded) that are multiples of 3 or 5.
- * 
+ *
+ * Description:  Sums all multiiples of 3 or 5 below 1024 that are natural nos.
+ *
  * Return: 0 on success
  */
 
 int main(void)
 {
-	int i, sum;
-	
+	int i;
+	int sum = 0;
+
 	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3) == 0 || (i % 5) == 0)
-		{
-			sum += i;
-		}
-		printf("%d\n", sum);
+		if ((i % 5) == 0 || (i % 3) == 0)
+		{sum += i; }
 	}
+	print("%d\n", sum);
 	return (0);
 }
