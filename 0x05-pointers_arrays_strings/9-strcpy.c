f706b4b07513db159fa8f7afe357ed92beb786e6
InKiOn <122981923+InKiOn@users.noreply.github.com>
2023-02-27 19:41:30 +0000

    Copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,15 +14,13 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; src[i] != '\n'; i++)
+	i = 0;
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	if (src[i] == '\n')
-	{
-		dest [i] = src[i];
-	}
+	dest[i] = '\0';
 
 	return (dest);
 }
