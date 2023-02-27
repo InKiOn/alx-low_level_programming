f46ad1e2ad1b74327accc4bd28c61cb182acc532
InKiOn <122981923+InKiOn@users.noreply.github.com>
2023-02-27 14:02:58 +0000

    Betty fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -23,7 +23,7 @@ void rev_string(char *s)
 		{
 			n = i;
 		}
-		for (c = 0; c <= ((n+1)/2); c++)
+		for (c = 0; c <= ((n + 1) / 2); c++)
 		{
 			k = s[n];
 			s[n] = t[c];
@@ -31,6 +31,5 @@ void rev_string(char *s)
 			n--;
 			c++;
 		}
-		_putchar('\n');
 	}
 }
