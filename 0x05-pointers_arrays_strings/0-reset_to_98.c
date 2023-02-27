517d930aba6caf44b6bf248a21146f2da67bb63d
InKiOn <122981923+InKiOn@users.noreply.github.com>
2023-02-27 10:29:42 +0000

    Betty styling fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * reset_to_98: void function
- * @*n: pointer value of int type
+ * reset_to_98 - void function
+ * @n: pointer value of int type
  *
  * Description: Updates the value pointed to to 98
  */
 
 void reset_to_98(int *n)
-{ 
+{
 	*n = 98;
 }
