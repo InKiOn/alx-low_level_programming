d59ca54129ef4ba4716b55108732514d70bc33cd
InKiOn <122981923+InKiOn@users.noreply.github.com>
2023-02-27 16:40:01 +0000

    Print half last

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * puts_half - void function
+ * @str: char type to print
+ *
+ * Description: print half the of a string
+ *
+ */
+
+void puts_half(char *str)
+{
+	int i, j, n;
+	
+	if (*str == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			j++;
+		}
+		if (i % 2 == 0)
+		{
+			for (j = i - 1; j >= (i/2); j--)
+			{
+				_putchar(str[j]);
+			}
+		}
+		else
+		{
+			n = (i - 1) / 2;
+			for (j = i - 1; j >= n; j--)
+			{
+				_putchar(str[j]);
+			}
+		}
+	}
+}
