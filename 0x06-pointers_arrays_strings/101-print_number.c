c6388fcdde1937852d24e2f30cef2f820a45f1bc
InKiOn <122981923+InKiOn@users.noreply.github.com>
2023-03-01 16:50:33 +0000

    Print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,18 +9,20 @@
 
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int i;
+
+	i = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -num;
+		i = -n;
 	}
 
-	if ((num / 10) > 0)
+	if ((i / 10) != 0)
 	{
-		print_number(num / 10);
+		print_number(i / 10);
 	}
 
-	_putchar((num % 10) + '0');
+	_putchar((i % 10) + '0');
 }
