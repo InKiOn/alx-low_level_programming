28551d3a9bde24f68de3da74159494a9eb092db9
InKiOn <122981923+InKiOn@users.noreply.github.com>
2023-03-01 16:41:31 +0000

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,15 +16,11 @@ char *_strncpy(char *dest, char *src, int n)
 	int i = 0;
 	int j = 0;
 
-	while (src[i++])
+	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
-		j++;
+		dest[i] = src[i];
 	}
-	for (i = 0; src[i] && i < n; i++)
-	{
-		dest[j++] = src[i];
-	}
-	for (i = j; i < n; i++)
+	for (; i < n; i++)
 	{
 		dest[i] = '\0';
 	}
