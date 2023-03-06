45542291629b7f526f09eecb7bbcdecb5ce78151
InKiOn <122981923+InKiOn@users.noreply.github.com>
2023-03-06 20:59:03 +0000

    betty fixes

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,13 +13,13 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int index;
-	unsigned int bytes;
+	unsigned int bytes = 0;
 
 	while (*s)
 	{
 		for (index = 0; accept[index]; index++)
 		{
-			if(*s == accept[index])
+			if (*s == accept[index])
 			{
 				bytes++;
 				break;
