e510624d177692d1a388c4144525c3e19ecd420a
InKiOn <122981923+InKiOn@users.noreply.github.com>
2023-03-06 20:40:35 +0000

    Memset fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,12 +13,12 @@
 
 char *_memset(char *s, char *b, unsigned int n)
 {
-	int index = 0;
+	int index;
 
-	while (index < n)
+	for (index = 0; n > 0; index++)
 	{
 		s[index] = b;
-		index++;
+		n--;
 	}
 
 	return (s);
