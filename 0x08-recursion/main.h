035c7c00262782b547e8d3b12c6781d73cc3f8cc
InKiOn <122981923+InKiOn@users.noreply.github.com>
2023-03-08 20:17:13 +0000

    Task push

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,8 +8,8 @@
  * Description: Header file containing function prototypes
  */
 
-int _putchar(char c);
 void _puts_recursion(char *s);
+int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
